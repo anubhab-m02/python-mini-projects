115f98338ec09dbb5fb9c5971f0516de2aff10e8
Anubhab Mishra <anubhab.m248@gmail.com>
2024-01-02 13:20:02 +0530

    Update

---
+++ Number-Guessing-Game/README.md
@@ -0,0 +1,25 @@
+# Number Guessing Game
+
+## Overview
+
+The Number Guessing Game is a simple Python script where the computer thinks of a random number between 1 and 100, and the user tries to guess it. The user is given a limited number of attempts to guess the correct number.
+
+## How to Play
+
+1. Run the script using a Python interpreter.
+
+    ```bash
+    python number_guessing_game.py
+    ```
+
+2. The game will prompt you to choose the difficulty level:
+   - Type 'easy' for 10 turns.
+   - Type 'hard' for 5 turns.
+
+3. The computer will randomly select a number between 1 and 100.
+
+4. You will be prompted to make a guess. Enter your guess.
+
+5. The game will provide feedback on whether your guess is too high, too low, or correct.
+
+6. Keep guessing until you either guess the correct number or run out of turns.
--- Number-Guessing-Game/main.py
+++ Number-Guessing-Game/main.py
@@ -1,9 +1,10 @@
 from random import randint
 
+# Constants for the number of turns in different difficulty levels
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
-#checks the user answer
+# Checks the user's answer and returns the updated number of turns
 def check_answer(guess, answer, turns):
     if guess > answer:
         print("Too high.")
@@ -14,7 +15,7 @@ def check_answer(guess, answer, turns):
     else:
         print(f"You got it! The answer was {answer}.")
 
-#sets the number of chances the user gets to 
+# Sets the difficulty level and returns the corresponding number of turns
 def set_difficulty():
     level = input("Choose the number of turns. Type 'easy' for 10 turns or 'hard' for 5 turns: ")
     if level == "easy":
@@ -24,10 +25,11 @@ def set_difficulty():
     else:
         print("Error!")
 
+# Main game function
 def game():
-    print("The Number Guessing Game! \nThe computer thinks of a number, you win if you guess the number!")
-    turns = set_difficulty()
-    answer = randint(1,100)
+    print("The Number Guessing Game!\nThe computer thinks of a number, you win if you guess the number!")
+    turns = set_difficulty()  # Set the difficulty level and get the initial number of turns
+    answer = randint(1, 100)  # Generate a random number between 1 and 100
     print("The number is between 1 and 100.")
 
     guess = 0
@@ -35,13 +37,14 @@ def game():
         print(f"You have {turns} attempts remaining to guess the number.")
         guess = int(input("Make a guess: "))
 
-        #no of turns remaining for the user to guess
+        # Check the user's guess and update the number of turns
         turns = check_answer(guess, answer, turns)
+
         if turns == 0:
             print(f"You've run out of guesses, you lose. The right number was {answer}.")
             return
         elif guess != answer:
             print("Guess again.")
-        
 
+# Run the game
 game()
